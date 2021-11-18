7ca5f1f25b5d7ee8af48f6612fbe300288b8ff32
andora814 <smith.doran@gmail.com>
2021-11-18 15:15:21 -0500

    clicking card results in a 1 populating

--- memory.js
+++ memory.js
@@ -1,1 +1,17 @@
 console.log('test');
+
+const cards = document.querySelector('.card');
+const buttons = document.querySelectorAll('.button');
+
+// Event Listeners
+
+function cardClicked() {
+  for (let i = 0; i < buttons.length; i++) {
+    buttons[i].addEventListener('click', () => {
+      buttons[i].innerText = '1';
+      console.log('button has been clicked');
+    });
+  }
+}
+
+cardClicked();
