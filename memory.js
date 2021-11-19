6ee74ca941d7321673011547d771ec767092d79d
andora814 <smith.doran@gmail.com>
2021-11-19 11:08:34 -0500

    only allows two clicks

--- memory.js
+++ memory.js
@@ -1,12 +1,8 @@
 //////// Test ////////
-console.log('test');
+// console.log('test');
 
 //////// Global Variables ////////
 const cards = document.querySelector('.card');
-const cardOne = document.querySelector('#cardOne');
-const cardTwo = document.querySelector('#cardTwo');
-const cardThree = document.querySelector('#cardThree');
-const cardFour = document.querySelector('#cardFour');
 const buttons = document.querySelectorAll('.button');
 let match = false;
 let playOver = false;
@@ -16,11 +12,8 @@ let firstCardClicked = false;
 let secondCardClicked = false;
 let cardImages = ['dog', 'dog', 'cat', 'cat'];
 
-console.log(cardImages);
+//////// Functions ////////
 
-// Example of how to shuffle an array:
-// https://dev.to/codebubb/how-to-shuffle-an-array-in-javascript-2ikj
-// Math.floor rounds down to nearest integer
 const shuffleArray = (cardImages) => {
   for (let i = cardImages.length - 1; i > 0; i--) {
     const j = Math.floor(Math.random() * (i + 1));
@@ -29,20 +22,29 @@ const shuffleArray = (cardImages) => {
     cardImages[j] = temp;
   }
 };
-shuffleArray(cardImages);
-console.log(cardImages);
 
-//////// Event Listeners ////////
+console.log('ORIGINAL: ' + cardImages);
+
+shuffleArray(cardImages);
+
+console.log('SHUFFLED: ' + cardImages);
 
 function cardClicked() {
   for (let i = 0; i < buttons.length; i++) {
     buttons[i].addEventListener('click', () => {
-      console.log('button has been clicked');
-      buttons[i].innerText = cardImages[i];
-      clickCount++;
-      console.log(clickCount);
+      if (clickCount < 2) {
+        buttons[i].innerText = cardImages[i];
+        clickCount++;
+        console.log('clickCount is ' + clickCount);
+      } else {
+        playOver = true;
+      }
     });
   }
 }
 
 cardClicked();
+
+// Example of how to shuffle an array:
+// https://dev.to/codebubb/how-to-shuffle-an-array-in-javascript-2ikj
+// Math.floor rounds down to nearest integer
