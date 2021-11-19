6afd20577cb2eaa1e20d564543e7b8eccd2032b6
andora814 <smith.doran@gmail.com>
2021-11-19 10:01:42 -0500

    cardClicked function simplified

--- memory.js
+++ memory.js
@@ -8,12 +8,18 @@ const cardTwo = document.querySelector('#cardTwo');
 const cardThree = document.querySelector('#cardThree');
 const cardFour = document.querySelector('#cardFour');
 const buttons = document.querySelectorAll('.button');
+let match = false;
+let playOver = false;
+let gameOver = false;
+let clickCount = 0;
+let firstCardClicked = false;
+let secondCardClicked = false;
 let cardImages = ['dog', 'dog', 'cat', 'cat'];
 
 console.log(cardImages);
 
 // Example of how to shuffle an array:
-// https://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array
+// https://dev.to/codebubb/how-to-shuffle-an-array-in-javascript-2ikj
 // Math.floor rounds down to nearest integer
 const shuffleArray = (cardImages) => {
   for (let i = cardImages.length - 1; i > 0; i--) {
@@ -31,19 +37,10 @@ console.log(cardImages);
 function cardClicked() {
   for (let i = 0; i < buttons.length; i++) {
     buttons[i].addEventListener('click', () => {
-      if (i === 0) {
-        console.log('button one has been clicked');
-        buttons[i].innerText = cardImages[0];
-      } else if (i === 1) {
-        console.log('button two has been clicked');
-        buttons[i].innerText = cardImages[1];
-      } else if (i === 2) {
-        console.log('button three has been clicked');
-        buttons[i].innerText = cardImages[2];
-      } else if (i === 3) {
-        console.log('button four has been clicked');
-        buttons[i].innerText = cardImages[3];
-      }
+      console.log('button has been clicked');
+      buttons[i].innerText = cardImages[i];
+      clickCount++;
+      console.log(clickCount);
     });
   }
 }
