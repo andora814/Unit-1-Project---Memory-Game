ab156b5019d7248d97d6f49860f094e53a8e693c
andora814 <smith.doran@gmail.com>
2021-11-19 16:26:35 -0500

    first two card clicked are pushed to doneArray

--- memory.js
+++ memory.js
@@ -4,15 +4,36 @@
 //////// Global Variables ////////
 const cards = document.querySelector('.card');
 const buttons = document.querySelectorAll('.button');
+const replay = document.querySelector('#replay');
+let matchMessage = document.querySelector('#matchMessage');
+// const imageOne = document.querySelector('#imageOne');
+// imageOne.src =
+//   'https://cdn.vectorstock.com/i/thumb-large/78/18/cute-carrot-character-isolated-element-vector-31257818.webp';
+// const imageTwo = document.querySelector('#imageTwo');
+// const imageThree = document.querySelector('#imageThree');
+// const imageFour = document.querySelector('#imageFour');
 let match = false;
 let playOver = false;
 let gameOver = false;
 let clickCount = 0;
 let firstCardClicked = '';
 let secondCardClicked = '';
+
 let cardImages = ['dog', 'dog', 'cat', 'cat'];
+let doneArray = [];
+let blankArray = ['', '', '', '', ''];
+// let doneArray = ['done', 'done', 'done', 'done'];
+let flippedCards = [firstCardClicked, secondCardClicked];
 
 //////// Functions ////////
+function playAgain() {
+  replay.addEventListener('click', () => {
+    buttons[0].innerText = '';
+    buttons[1].innerText = '';
+    buttons[2].innerText = '';
+    buttons[3].innerText = '';
+  });
+}
 
 function shuffleArray(cardImages) {
   for (let i = cardImages.length - 1; i > 0; i--) {
@@ -29,7 +50,10 @@ console.log('SHUFFLED: ' + cardImages);
 function cardsMatch() {
   if (firstCardClicked === secondCardClicked) {
     match = true;
+    // what does the red circle mean?
     return;
+    matchMessage.innerText = 'You have a match!';
+    // why is this "unreachable code?"
   }
   match = false;
   return;
@@ -43,6 +67,7 @@ function cardClicked() {
         clickCount++;
         if (clickCount === 1) {
           console.log(`clickCount is ${clickCount}`);
+          // what does the red circle mean?
           firstCardClicked = cardImages[i];
           console.log(`firstCardClicked is ${firstCardClicked}`);
         }
@@ -51,7 +76,17 @@ function cardClicked() {
           secondCardClicked = cardImages[i];
           console.log(`secondCardClicked is ${secondCardClicked}`);
           cardsMatch();
-          console.log(match);
+          if (match === true) {
+            doneArray.push(firstCardClicked);
+            doneArray.push(secondCardClicked);
+            console.log(firstCardClicked, secondCardClicked);
+            console.log(doneArray);
+            return;
+          } else {
+            firstCardClicked = blankArray[i];
+            secondCardClicked = blankArray[i];
+            console.log(match);
+          }
         }
       } else {
         playOver = true;
@@ -61,7 +96,22 @@ function cardClicked() {
 }
 
 cardClicked();
+playAgain();
 
 // Example of how to shuffle an array:
 // https://dev.to/codebubb/how-to-shuffle-an-array-in-javascript-2ikj
 // Math.floor rounds down to nearest integer
+
+// 12 cartoon foods:
+// carrot: https://cdn.vectorstock.com/i/thumb-large/78/18/cute-carrot-character-isolated-element-vector-31257818.webp
+// onion: https://cdn5.vectorstock.com/i/thumb-large/33/74/cute-smiling-onion-isolated-colorful-vector-31303374.jpg
+// avocado: https://cdn3.vectorstock.com/i/thumb-large/99/32/kawaii-cute-avocado-with-a-smile-vector-27289932.jpg
+// peach: https://cdn2.vectorstock.com/i/thumb-large/97/71/cute-and-happy-orange-character-vector-32319771.jpg
+// strawberry: https://cdn.vectorstock.com/i/thumb-large/34/72/cute-funny-strawberry-isolated-on-white-background-vector-31533472.webp
+// peas: https://cdn.vectorstock.com/i/thumb-large/90/53/cute-cartoon-peas-isolated-on-white-background-vector-31909053.webp
+// grapes: https://cdn.vectorstock.com/i/thumb-large/19/58/grapes-cute-kawaii-character-vector-29111958.webp
+// banana: https://cdn.vectorstock.com/i/thumb-large/93/94/cute-banana-with-happy-funny-face-tropical-fruit-vector-40089394.webp
+// broccoli: https://cdn.vectorstock.com/i/thumb-large/25/56/cute-cartoon-broccoli-vector-22022556.webp
+// egg: https://cdn.vectorstock.com/i/thumb-large/95/42/egg-icon-kawaii-logo-a-food-vector-38099542.webp
+// almond: https://cdn.vectorstock.com/i/thumb-large/44/39/cute-almond-on-a-white-background-vector-35664439.webp
+// lemon: https://cdn.vectorstock.com/i/thumb-large/20/29/cute-smiling-lemon-isolated-colorful-fruit-vector-28362029.webp
